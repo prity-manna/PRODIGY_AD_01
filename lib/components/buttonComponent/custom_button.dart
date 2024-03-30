4f1b2cf47e88a375695b8226812a714b7335890e
chandandas9353r <chandandas9353r@gmail.com>
2024-03-31 03:42:34 +0530

    Basic Calculator with All Functionalities

--- lib/components/buttonComponent/custom_button.dart
+++ lib/components/buttonComponent/custom_button.dart
@@ -1,31 +1,59 @@
 import 'package:flutter/material.dart';
 
-class CustomButton extends StatelessWidget {
+class CustomButton extends StatefulWidget {
   late final String _buttonText;
-  late final Function(String text) onTapActivity;
-  CustomButton(
-      {super.key, required String buttonText, required this.onTapActivity}) {
+  final Function(String text) onTapActivity;
+  CustomButton({super.key, required String buttonText, required this.onTapActivity}) {
     _buttonText = buttonText;
+  }
+
+  @override
+  State<CustomButton> createState() => _CustomButtonState();
+}
+
+class _CustomButtonState extends State<CustomButton> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 200));
+    _scaleAnimation = Tween<double>(begin: 0.95, end: 1.0).animate(_animationController);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => onTapActivity(_buttonText),
-      child: Container(
-        width: MediaQuery.of(context).size.width * .2,
-        height: MediaQuery.of(context).size.height * .05,
-        padding: const EdgeInsets.all(0),
-        margin: const EdgeInsets.all(0),
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          color: Colors.green,
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: Text(
-          _buttonText,
-          textAlign: TextAlign.center,
-          style: const TextStyle(color: Colors.white, fontSize: 30),
+      onTap: () {
+        _animationController.forward();
+        widget.onTapActivity(widget._buttonText);
+      },
+      onTapDown: (_) => _animationController.reverse(),
+      onTapCancel: () => _animationController.reverse(),
+      child: ScaleTransition(
+        scale: _scaleAnimation,
+        child: Container(
+          width: MediaQuery.of(context).size.width * .2,
+          height: MediaQuery.of(context).size.height * .05,
+          padding: const EdgeInsets.all(0),
+          margin: const EdgeInsets.all(0),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Text(
+            widget._buttonText,
+            textAlign: TextAlign.center,
+            style: const TextStyle(color: Colors.white, fontSize: 30),
+          ),
         ),
       ),
     );
--- lib/services/calculator_button_functions.dart
+++ lib/services/calculator_button_functions.dart
@@ -1,3 +1,125 @@
+import 'dart:async';
+
 class ButtonFunctions {
-  static void onTapNumber(String number) {}
+
+  static Future<bool> isDecimal(String val) async => val == ".";
+
+  static Future<bool> isOperator(String val) async {
+    return val == "/" || val == "x" || val == "+" || val == "-";
+  }
+
+  static Future<bool> _isNumeric(String? str) async {
+    if (str == null) {
+      return false;
+    }
+    return double.tryParse(str) != null;
+  }
+
+  static Future<int> _getPrecedence(String op) async {
+    switch (op) {
+      case '+':
+      case '-':
+        return 1;
+      case 'x':
+      case '/':
+        return 2;
+      default:
+        return 0;
+    }
+  }
+
+  static Future<bool> _hasHigherPrecedence(String op1, String op2) async {
+    int precedence1 = await _getPrecedence(op1);
+    int precedence2 = await _getPrecedence(op2);
+    return precedence1 >= precedence2;
+  }
+
+  static Future<List<String>> _convertToPostfix(List<String> infixExpression) async {
+    List<String> postfix = [];
+    List<String> operatorStack = [];
+
+    for (String token in infixExpression) {
+      if (await _isNumeric(token)) {
+        postfix.add(token);
+      } else if (await isOperator(token)) {
+        while (operatorStack.isNotEmpty && await _hasHigherPrecedence(operatorStack.last, token)) {
+          postfix.add(operatorStack.removeLast());
+        }
+        operatorStack.add(token);
+      }
+    }
+  
+    while (operatorStack.isNotEmpty) {
+      postfix.add(operatorStack.removeLast());
+    }
+  
+    return postfix;
+  }
+
+  static Future<double> _performOperation(double operand1, double operand2, String operator) async {
+    switch (operator) {
+      case '/':
+        return operand1 / operand2;
+      case 'x':
+        return operand1 * operand2;
+      case '+':
+        return operand1 + operand2;
+      case '-':
+        return operand1 - operand2;
+      default:
+        throw ArgumentError('Invalid operator: $operator');
+    }
+  }
+
+  static Future<double> _evaluatePostfix(List<String> postfixExpression) async {
+    List<double> stack = [];
+
+    for (String token in postfixExpression) {
+      if (await _isNumeric(token)) {
+        stack.add(double.parse(token));
+      } else if (await isOperator(token)) {
+        double operand2 = stack.removeLast();
+        double operand1 = stack.removeLast();
+        double result = await _performOperation(operand1, operand2, token);
+        stack.add(result);
+      }
+    }
+
+    return stack.first;
+  }
+
+  static Future<Object?> onCalculation(String inputText) async {
+    double? result;
+    try {
+      List<String> tokens = [];
+      String currentNumber = "";
+
+      for (int i = 0; i < inputText.length; i++) {
+        String val = inputText[i];
+        if (await isOperator(val)) {
+          if (i == 0 || await isOperator(inputText[i - 1])) {
+            currentNumber += val;
+          } else {
+            tokens.add(currentNumber);
+            tokens.add(val);
+            currentNumber = '';
+          }
+        } else {
+          currentNumber += val;
+          if (i == inputText.length - 1) {
+            tokens.add(currentNumber);
+          }
+        }
+      }
+
+      List<String> postfixExpression = await _convertToPostfix(tokens);
+      result = double.parse((await _evaluatePostfix(postfixExpression)).toStringAsFixed(10));
+      int resultInt = result.toInt();
+      if (resultInt == result) return resultInt;
+    } catch (ex) {
+      // Error
+    }
+    return result;
+  }
+
 }
